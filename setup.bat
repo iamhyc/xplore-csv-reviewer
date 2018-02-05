77826a687ad513ee6ad221b0748adfebb13bb9b7
iamhyc <sudofree@163.com>
2018-02-05 19:05:25 +0800

    ready to change to curses version

--- setup.bat
+++ setup.bat
@@ -1,6 +1,9 @@
 
 pip install termcolor
 pip install pynput
+pip install wheel
+
+pip install curses-2.2-cp27-none-win32.whl
 
 @echo ======================== Complete ========================
 @pause
